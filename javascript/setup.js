e9c3fbbe2c1693deece9e6c892f2fade752cb222
Edwin A <ea976@nyu.edu>
2016-02-15 15:21:45 -0500

    TDD code only

--- javascript/setup.js
+++ javascript/setup.js
@@ -1,3 +1,22 @@
-function compliment(){
-  return "You look nice today"
-}
+function fizzBuzzWoof(n){
+	var num = n.toString();
+	var ans= "";
+	if (containOrDivisible(3)) {
+		ans += "Fizz";
+	};
+	if (containOrDivisible(5)) {
+		ans += "Buzz";
+	};
+	if (containOrDivisible(7)) {
+		ans += "Woof";
+	};
+	if(!ans) {
+		return n;
+	};
+  return ans;
+
+
+function containOrDivisible(testNumber) {
+	return num.indexOf(testNumber) !==-1 || n % testNumber ===0;
+	};
+};
--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,10 +1,42 @@
-describe('Setting up tests with Jasmine', function(){
-  it('should be true', function() {
-    expect(true).toBe(true);
+describe('FBW Testing -', function(){
+  it('argument 3 should return Fizz', function() {
+    expect(fizzBuzzWoof(3)).toBe("Fizz");
   });
+  it('divisable by 3 should return Fizz', function() {
+  	expect(fizzBuzzWoof(6)).toBe("Fizz");
+  })
+    it('argument 5 should return Buzz', function() {
+  	expect(fizzBuzzWoof(5)).toBe("Buzz");
+  })
+    it('argument 1 should return 1', function() {
+    	expect(fizzBuzzWoof(1)).toBe(1);
+    })
+        it('argument 13 should return Fizz', function() {
+    	expect(fizzBuzzWoof(13)).toBe("Fizz");
+    })
+    it('argument 5 should return Buzz', function() {
+       	expect(fizzBuzzWoof(15)).toBe("FizzBuzz");
+       	     })
+    it('argument 53 should return FizzBuzz', function() {
+    	expect(fizzBuzzWoof(53)).toBe("FizzBuzz");
+    })
 
-  it('shold see the source file', function() {
-    expect(compliment()).toBe('You look nice today');
-  });
+     it('argument 7 should return Woof', function() {
+    	expect(fizzBuzzWoof(7)).toBe("Woof");
+    })
+     it('argument 47 should return Woof', function() {
+     	expect(fizzBuzzWoof(71)).toBe("Woof");
+     })
+     it('argument 35 should return FizzBuzzWoof', function() {
+     	expect(fizzBuzzWoof(35)).toBe("FizzBuzzWoof");
+     })
+     it('argument 37 should return FizzWoof', function() {
+     	expect(fizzBuzzWoof(37)).toBe("FizzWoof");
+     })
 });
+
+
+
+
+
 
